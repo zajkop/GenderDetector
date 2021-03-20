f49f683f24ae38dd2d3447eefa780afbc696643b
Patryk Zajko <pzajko@ptc.com>
2021-03-20 21:50:08 +0100

    Tokens searching in files implemented

--- src/main/java/com/silenteight/genderdetector/utility/TokenFileReader.java
+++ src/main/java/com/silenteight/genderdetector/utility/TokenFileReader.java
@@ -1,9 +1,44 @@
 package com.silenteight.genderdetector.utility;
 
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+@Component
 public class TokenFileReader {
 
-    private static final String MALE_TOKENS_FILE_PATH = "";
-    private static final String FEMALE_TOKENS_FILE_PATH = "";
+    public boolean isTokenInSpecifiedFile(String token, String filePath) {
+        Scanner scanner = new Scanner(getFileFromResourcesAsInputStream(filePath));
+        while (scanner.hasNextLine()) {
+            String tokenFromFile = scanner.nextLine();
+            if (tokenFromFile.equals(token)) {
+                return true;
+            }
+        }
+        scanner.close();
+        return false;
+    }
 
-    
+    public List<String> getCorrespondingTokensInSpecifiedFile(List<String> tokens, String filePath) {
+        List<String> foundedTokens = new ArrayList<>();
+        Scanner scanner = new Scanner(getFileFromResourcesAsInputStream(filePath));
+        while (scanner.hasNextLine()) {
+            String token = scanner.nextLine();
+            if (tokens.contains(token)) {
+                foundedTokens.add(token);
+            }
+        }
+        scanner.close();
+        return foundedTokens;
+    }
+
+    private InputStream getFileFromResourcesAsInputStream(String filePath) {
+        return getClass()
+                .getClassLoader()
+                .getResourceAsStream(filePath);
+    }
+
 }
