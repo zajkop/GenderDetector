2d8156ba2137e7b093861c4f89ca2b6f186efd2e
Patryk Zajko <pzajko@ptc.com>
2021-03-21 15:19:05 +0100

    TokenFileReaderTest improved

--- src/test/java/com/silenteight/genderdetector/utility/TokenFileReaderTest.java
+++ src/test/java/com/silenteight/genderdetector/utility/TokenFileReaderTest.java
@@ -4,8 +4,13 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+/**
+ * TokenFileReader test class
+ */
 class TokenFileReaderTest {
 
     private static final String MALE_TOKENS_FILE = "tokens/male-test-tokens.txt";
@@ -13,11 +18,17 @@ class TokenFileReaderTest {
     private static final String FEMALE_NAME = "Martyna";
     private static TokenFileReader tokenFileReader;
 
+    /**
+     * Creates TokenFileReader before tests execution
+     */
     @BeforeAll
     public static void setUp() {
         tokenFileReader = new TokenFileReader();
     }
 
+    /**
+     * Verifies that token is present specified file
+     */
     @Test
     public void verifyThatTokenIsInSpecifiedFile() {
         Assertions.assertThat(tokenFileReader.isTokenInSpecifiedFile(MALE_NAME, MALE_TOKENS_FILE))
@@ -25,6 +36,9 @@ class TokenFileReaderTest {
                 .isTrue();
     }
 
+    /**
+     * Verifies that token is not present in specified file
+     */
     @Test
     public void verifyThatTokenIsNotInSpecifiedFile() {
         Assertions.assertThat(tokenFileReader.isTokenInSpecifiedFile(FEMALE_NAME, MALE_TOKENS_FILE))
@@ -32,6 +46,9 @@ class TokenFileReaderTest {
                 .isFalse();
     }
 
+    /**
+     * Verifies that corresponding to input tokens are present in specified file
+     */
     @Test
     public void verifyThatCorrespondingTokensAreInSpecifiedFile() {
         List<String> inputTokens = List.of(MALE_NAME, FEMALE_NAME);
@@ -39,5 +56,16 @@ class TokenFileReaderTest {
         Assertions.assertThat(correspondingTokensFromFile)
                 .as("Only %s token should be found in file", MALE_NAME)
                 .containsOnly(MALE_NAME);
+    }
+
+    /**
+     * Checks if input stream is retrieved properly
+     *
+     * @throws IOException when can't get token's input stream
+     */
+    @Test
+    public void shouldRetrieveTokensAsInputStream() throws IOException {
+        InputStream tokensForEachGender = tokenFileReader.getTokensForEachGender();
+        Assertions.assertThat(tokensForEachGender.readAllBytes()).as("Tokens stream should contains data").isNotEmpty();
     }
 }
